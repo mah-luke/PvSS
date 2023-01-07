dbc9ce28c26adfdcc90c41dc212bf41129636d3d
Lukas <lukas.mah@outlook.at>
2023-01-07 13:35:34 +0100

    add constructor for RiskAgentMcts

--- src/main/java/fun/agent/RiskAgentMcts.java
+++ src/main/java/fun/agent/RiskAgentMcts.java
@@ -2,6 +2,7 @@ package fun.agent;
 
 import at.ac.tuwien.ifs.sge.agent.AbstractGameAgent;
 import at.ac.tuwien.ifs.sge.agent.GameAgent;
+import at.ac.tuwien.ifs.sge.engine.Logger;
 import at.ac.tuwien.ifs.sge.game.risk.board.Risk;
 import at.ac.tuwien.ifs.sge.game.risk.board.RiskAction;
 
@@ -9,6 +10,16 @@ import java.util.concurrent.TimeUnit;
 
 public class RiskAgentMcts extends AbstractGameAgent<Risk, RiskAction> implements GameAgent<Risk, RiskAction> {
 
+    public RiskAgentMcts(Logger log) {
+    /* instantiates a AbstractGameAgent so that shouldStopComputation() returns true after 3/4ths of
+     * the given time. However, if it can, it will try to compute at least 5 seconds.
+     */
+        super(3D / 4D, 5, TimeUnit.SECONDS, log);
+
+    //Note: log can be effectively ignored, however it honors the flags of the engine.
+
+    //Do some setup before the TOURNAMENT starts.
+  }
 
     @Override
     public void setUp(int numberOfPlayers, int playerId) {
