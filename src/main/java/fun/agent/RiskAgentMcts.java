ea1bd1653a06656e9c189c408ee0dba968b5a6d9
Lukas <lukas.mah@outlook.at>
2023-01-07 13:37:25 +0100

    change foramtting of RiskAgentMcts to 4 space indent

--- src/main/java/fun/agent/RiskAgentMcts.java
+++ src/main/java/fun/agent/RiskAgentMcts.java
@@ -11,15 +11,15 @@ import java.util.concurrent.TimeUnit;
 public class RiskAgentMcts extends AbstractGameAgent<Risk, RiskAction> implements GameAgent<Risk, RiskAction> {
 
     public RiskAgentMcts(Logger log) {
-    /* instantiates a AbstractGameAgent so that shouldStopComputation() returns true after 3/4ths of
-     * the given time. However, if it can, it will try to compute at least 5 seconds.
-     */
+        /* instantiates a AbstractGameAgent so that shouldStopComputation() returns true after 3/4ths of
+         * the given time. However, if it can, it will try to compute at least 5 seconds.
+         */
         super(3D / 4D, 5, TimeUnit.SECONDS, log);
 
-    //Note: log can be effectively ignored, however it honors the flags of the engine.
+        //Note: log can be effectively ignored, however it honors the flags of the engine.
 
-    //Do some setup before the TOURNAMENT starts.
-  }
+        //Do some setup before the TOURNAMENT starts.
+    }
 
     @Override
     public void setUp(int numberOfPlayers, int playerId) {
